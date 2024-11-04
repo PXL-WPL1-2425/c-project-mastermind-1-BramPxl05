8cddb909b732b38cd58a7964fa5bb60942471d6d
BramPetersPXL <12401770@student.pxl.be>
2024-11-04 16:13:11 +0100

    MasterMind-02 Code Er wordt een random code van 4 kleuren gegenereerd. Om het spel makkelijker te testen wordt deze code in de titel van het window getoond. De 4 kleuren worden willekeurig gekozen uit een reeks van 6 beschikbare kleuren: rood, geel, oranje, wit, groen en blauw.

--- mastermind2.0/mastermind2.0/MainWindow.xaml.cs
+++ mastermind2.0/mastermind2.0/MainWindow.xaml.cs
@@ -27,38 +27,128 @@ namespace mastermind2._0
             int kleur3 = rnd.Next(1, 7);
             int kleur4 = rnd.Next(1, 7);
 
-            string kleurtext1 = Convert.ToString(kleur1);
-            string kleurtext2 = Convert.ToString(kleur2);
-            string kleurtext3 = Convert.ToString(kleur3);
-            string kleurtext4 = Convert.ToString(kleur4);
+            //string kleurtext1 = Convert.ToString(kleur1);
+            //string kleurtext2 = Convert.ToString(kleur2);
+            //string kleurtext3 = Convert.ToString(kleur3);
+            //string kleurtext4 = Convert.ToString(kleur4);
+
+
+
+            string kleurtext1 = "";
+            string kleurtext2 = "";
+            string kleurtext3 = "";
+            string kleurtext4 = "";
+
+            //KLEUR 1
+
+            if (kleur1 == 1)
+            {
+                kleurtext1 = "rood";
+            }
+            else if (kleur1 == 2)
+            {
+                kleurtext1 = "geel";
+            }
+            else if (kleur1 == 3)
+            {
+                kleurtext1 = "oranje";
+            }
+            else if (kleur1 == 4)
+            {
+                kleurtext1 = "wit";
+            }
+            else if (kleur1 == 5)
+            {
+                kleurtext1 = "groen";
+            }
+            else if (kleur1 == 6)
+            {
+                kleurtext1 = "blauw";
+            }
+
+
+            //KLEUR 2
+
+            if (kleur2 == 1)
+            {
+                kleurtext2 = "rood";
+            }
+            else if (kleur2 == 2)
+            {
+                kleurtext2 = "geel";
+            }
+            else if (kleur2 == 3)
+            {
+                kleurtext2 = "oranje";
+            }
+            else if (kleur2 == 4)
+            {
+                kleurtext2 = "wit";
+            }
+            else if (kleur2 == 5)
+            {
+                kleurtext2 = "groen";
+            }
+            else if (kleur2 == 6)
+            {
+                kleurtext2 = "blauw";
+            }
+            
+            //KLEUR 3
+            
+            if (kleur3 == 1)
+            {
+                kleurtext3 = "rood";
+            }
+            else if (kleur3 == 2)
+            {
+                kleurtext3 = "geel";
+            }
+            else if (kleur3 == 3)
+            {
+                kleurtext3 = "oranje";
+            }
+            else if (kleur3== 4)
+            {
+                kleurtext3 = "wit";
+            }
+            else if (kleur3 == 5)
+            {
+                kleurtext3 = "groen";
+            }
+            else if (kleur3 == 6)
+            {
+                kleurtext3 = "blauw";
+            }
+
+            //KLEUR 4
+
+            if (kleur4 == 1)
+            {
+                kleurtext4 = "rood";
+            }
+            else if (kleur4 == 2)
+            {
+                kleurtext4 = "geel";
+            }
+            else if (kleur4 == 3)
+            {
+                kleurtext4 = "oranje";
+            }
+            else if (kleur4 == 4)
+            {
+                kleurtext4 = "wit";
+            }
+            else if (kleur4 == 5)
+            {
+                kleurtext4 = "groen";
+            }
+            else if (kleur4 == 6)
+            {
+                kleurtext4 = "blauw";
+            }
 
             this.Title = ($"{kleurtext1}, {kleurtext2}, {kleurtext3}, {kleurtext4}");
-            //if (kleur1 == 1)
-            //{
-            //    Convert.kl
-            //}
-            //else if (kleur1 == 2)
-            //{
-
-            //}
-            //else if (kleur1 == 3)
-            //{
-
-            //}
-            //else if (kleur1 == 4)
-            //{
-
-            //}
-            //else if (kleur1 == 5)
-            //{
-
-            //}
-            //else if (kleur1 == 6)
-            //{
-
-            //}
-
-
         }
     }
 }
